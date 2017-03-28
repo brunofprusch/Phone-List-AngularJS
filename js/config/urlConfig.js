c6f20a7c4e7f94ecf62c473387b888d25086e3cf
brunofprusch <brunoprusch@gmail.com>
2017-03-27 22:08:14 -0300

    Changes to use PRD ip.

--- js/config/urlConfig.js
+++ js/config/urlConfig.js
@@ -1,17 +1,17 @@
 angular.module("phoneList").value("urlConfig", {
 
-/*
   getAllContactsUrl: "http://159.203.220.124:8080/contact/all",
   addContantUrl: "http://159.203.220.124:8080/contact/add",
   getAllCarriersUrl: "http://159.203.220.124:8080/carrier/all",
   deleteContact: "http://159.203.220.124:8080/contact/",
   getContact: "http://159.203.220.124:8080/contact/"
-*/
- 
+
+ /*
   getAllContactsUrl: "http://127.0.0.1:8080/contact/all",
   addContantUrl: "http://127.0.0.1:8080/contact/add",
   getAllCarriersUrl: "http://127.0.0.1:8080/carrier/all",
   deleteContact: "http://127.0.0.1:8080/contact/",
   getContact: "http://127.0.0.1:8080/contact/"
+*/
 
 });
